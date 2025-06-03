0954e933515439ec7eee0bd20d129db048def117
oHeviaDeveloper <ohevia@uoc.edu>
2025-06-03 23:41:35 +0200

    Clean files and icons

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:8000/api'
+  apiUrlLocal: 'http://127.0.0.1:8000/api',
+  apiUrl: 'https://homing-api-production.up.railway.app/api'
 };
